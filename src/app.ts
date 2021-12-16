06e31abfc5e73965fa8e16e4b97654fb4ab964f4
heochristopher <christopher.heo23@sitechhs.com>
2021-12-16 11:44:31 -0500

    successful calls

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express"
 const port = process.env.PORT || 3000
 const app = express()
+import { router } from './routes/index'
 
+app.use('/', router)
 app.listen(port, ()=> {
     console.log(`server is up on port ${port}`)
 })
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,31 @@
-const bruh:string = 'yoyoyo'
-console.log(bruh)
+import express from 'express'
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    interface User {
+        readonly id: number,
+        name: string,
+        admin: boolean,
+        gender?: string
+    }
+    const newUser: User ={
+        id: 1234,
+        name: 'bobby',
+        admin: false,
+    }
+    try {
+        res.send(req.query)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/user/:id', async(req,res) => {
+    try {
+        res.json(req.params)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = {router}
