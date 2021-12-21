87bf1d6b9530532836d5de6e8807a1d81728c2f7
heochristopher <71462915+heochristopher@users.noreply.github.com>
2021-12-21 15:52:03 -0500

    figure out how to use dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express"
-
 import { router } from './routes/index'
+import './DB/mongoose'
 
 const port = process.env.PORT || 3000
 const app = express();
+
+//raw requests are now usable properties on req.body
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.use('/', router)
 app.listen(port, ()=> {
