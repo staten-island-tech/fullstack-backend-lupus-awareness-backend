5a31883582ff86c2f44fd69d7865aa4605fd2469
heochristopher <christopher.heo23@sitechhs.com>
2022-01-06 12:26:29 -0500

    make post model

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,44 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
-import { stringify } from 'querystring'
-import { isShorthandPropertyAssignment } from 'typescript'
 import { userModel } from '../models/User'
 const User = userModel
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.user) {
-        next()
-    } else {
-        res.json('sign in please')
-    }
-}
-
-export const authPage = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.json(`hello, ${req.body.user}`)
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const homePage = async (req: Request, res: Response) => {
-    interface User {
-        readonly id: number,
-        name: string,
-        admin: boolean,
-        gender?: string
-    }
-    const newUser: User ={
-        id: 1234,
-        name: 'bob',
-        admin: false,
-        gender: 'unspecified'
-    }
-    try {
-        res.json(newUser)
-    } catch (error) {
-        console.log(error)
-    }
-};
 
 export const createUser = async (req: Request, res: Response) => {
     try {
