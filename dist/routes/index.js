8dec1c25e048bc9fe19ef60b524145c08d427247
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-01-25 12:07:21 -0500

    Auth0 works

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = __importDefault(require("express"));
+const express_openid_connect_1 = require("express-openid-connect");
 const userController_1 = require("../controllers/userController");
 const router = express_1.default.Router();
 exports.router = router;
-router.get('/', userController_1.getUsers);
+router.get('/', (0, express_openid_connect_1.requiresAuth)(), userController_1.getUsers);
 router.post('/add', userController_1.createUser);
-router.post('/timesheets', userController_1.checkJwt, userController_1.testing); // Create timesheets API endpoint
+router.post('/timesheets', userController_1.testing); // Create timesheets API endpoint
 router.patch('/user/:id', userController_1.updateUsers);
 router.delete('/user/:id', userController_1.deleteUser);
