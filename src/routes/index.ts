ad07cbbf7496c1618ce35cafdc48ecb9965287b1
heochristopher <christopher.heo23@sitechhs.com>
2022-02-14 12:26:46 -0500

    uhhhhhhh

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,18 @@
 import express from 'express'
 import  { auth } from 'express-openid-connect'
 import { requiresAuth } from 'express-openid-connect'
+import { createEvent } from '../controllers/eventController'
 import { createUser, getUsers, updateUsers, deleteUser, getProfile} from '../controllers/userController'
 const router = express.Router()
 
 
 router.get('/',requiresAuth(), getUsers)
-router.patch('/user/:id', updateUsers)
+router.patch('/user/:id', requiresAuth(), updateUsers)
 router.delete('/user/:id', deleteUser)
 router.get('/profile', requiresAuth(), getProfile)
 router.get('/dashboard', requiresAuth(), getProfile)
 router.patch('/dashboard', requiresAuth(), updateUsers)
+router.post('/event', createEvent)
 
 
 
