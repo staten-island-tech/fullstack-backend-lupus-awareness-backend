daac06cc40b94a8be6ecf6ec6aa75c6c6d71dcfe
heochristopher <christopher.heo23@sitechhs.com>
2022-03-02 12:28:47 -0500

    asdf

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import { createUser, getUsers, updateUsers, deleteUser, getProfile} from '../con
 const router = express.Router()
 
 
-router.get('/', requiresAuth(),  getUsers)
+router.get('/', getUsers)
 router.patch('/user/:id', requiresAuth(), updateUsers)
 router.delete('/user/:id', deleteUser)
 router.get('/profile', requiresAuth(), getProfile)
