92d46ea80e32b0f315f77b6226fbecb01bf741e1
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-03-29 12:26:30 -0400

    organize router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,29 @@
 import express from 'express'
 import {getUsers, createUser, login, updateUsers, deleteUser, deleteAllUser, getProfile} from '../middleware/userMiddleware'
-import { createEvent, getEvents, createComment, reply, event} from '../middleware/eventMiddleware'
+import { createEvent, getEvents, createComment, reply, event, deleteEvent} from '../middleware/eventMiddleware'
 import {requiresAuth} from '../middleware/token'
 import {userJoi} from '../middleware/validation_schema'
 const router = express.Router()
 
 router.get('/', getUsers)
+router.get('/events', getEvents)
+
 router.post('/register', userJoi, createUser)
 router.post('/login', login)
-router.patch('/user/:id', updateUsers)
-router.delete('/user/:id', deleteUser)
 router.post('/event', requiresAuth, createEvent)
-router.get('/events', getEvents)
+
+router.patch('/user/:id', requiresAuth, updateUsers)
+
+
+router.delete('/user/:id', deleteUser)
 router.delete('/user', deleteAllUser)
+
+
+
 router.get('/profile', requiresAuth, getProfile)
 router.post('/event/:id/createComment',requiresAuth, createComment)
 router.post("/event/:event_id/comment/:comment_id/replyComment", requiresAuth, reply)
 router.get("/eventProfile/:id", event)
+router.delete('/event/:id', requiresAuth, deleteEvent)
 
 export {router}
