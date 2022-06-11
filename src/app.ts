0b354b11b661b81f31aa9e787ef4b98eca3c2565
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-11 12:10:30 -0400

    bn bn

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser())
 
 app.use(cors({ 
-  origin: 'https://https://altru.netlify.app',
+  origin: 'https://altru.netlify.app',
   credentials: true
 }))
 
