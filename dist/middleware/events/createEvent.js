302bbc92a2668389b706472b50c9d5a5afe9e21b
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-10 22:20:43 -0400

    working on images

--- dist/middleware/events/createEvent.js
+++ dist/middleware/events/createEvent.js
@@ -10,56 +10,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createEvent = void 0;
-const User_1 = require("../../models/User");
 const Event_1 = require("../../models/Event");
 const cloudinary = require("cloudinary").v2;
 const createEvent = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     // if(req.body.payload.role === Role.Student) {return res.status(400).json('Students cannot create events')}
     // console.log(req.files)
     try {
-        // const event = new Event({
-        //     user: req.body.payload,
-        //     name: req.body.name,
-        //     hours: req.body.hours,
-        //     start: req.body.start,
-        //     end: req.body.end,
-        //     location: req.body.location,
-        //     description: req.body.description,
-        //     media: req.files,
-        //     tags: req.body.tags
-        //     });
+        const media = [];
+        const event = new Event_1.Event({
+            user: req.body.payload,
+            name: req.body.name,
+            hours: req.body.hours,
+            start: req.body.start,
+            end: req.body.end,
+            location: req.body.location,
+            description: req.body.description,
+            media: media,
+            // tags: req.body.tags
+        });
         // await event.save();
         // await User.findOneAndUpdate({ _id: req.body.payload._id },
         // {$push: {events: event._id}}
         // );
         // res.json(event)
-        const imageFile = req.file;
+        const imageFile = req.file.path;
+        // res.json(imageFile)
         // const media = [];
         // const image = imageFiles?.map(image => {
         //   return image.path
         // })
         // res.json(image)
         cloudinary.uploader
-            .upload(imageFile.path, {
+            .upload(imageFile, {
             folder: "Fullstack/Event",
         }, function (error, result) {
             console.log(result, error);
         }).then((result) => __awaiter(void 0, void 0, void 0, function* () {
-            const event = new Event_1.Event({
-                user: req.body.payload,
-                name: req.body.name,
-                hours: req.body.hours,
-                start: req.body.start,
-                end: req.body.end,
-                location: req.body.location,
-                description: req.body.description,
-                media: result.url,
-                tags: req.body.tags
+            return new Promise((resolve, reject) => {
+                resolve(media.push(result.url));
             });
-            yield event.save();
-            yield User_1.User.findOneAndUpdate({ _id: req.body.payload._id }, { $push: { events: event._id } });
-            res.json(event);
+            // Promise.all(media).then(results => callback(results))
         }));
+        res.json(media);
         // imageFiles?.forEach((el: any) => {
         //   console.log(el.path);
         //   const file = el.path;
--- src/middleware/events/createEvent.ts
+++ src/middleware/events/createEvent.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { User, Role } from "../../models/User";
 import { Event } from "../../models/Event";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { callbackify } from "util";
 const cloudinary = require("cloudinary").v2;
 
 export const createEvent = async (
@@ -12,25 +13,27 @@ export const createEvent = async (
   // if(req.body.payload.role === Role.Student) {return res.status(400).json('Students cannot create events')}
   // console.log(req.files)
   try {
-    // const event = new Event({
-    //     user: req.body.payload,
-    //     name: req.body.name,
-    //     hours: req.body.hours,
-    //     start: req.body.start,
-    //     end: req.body.end,
-    //     location: req.body.location,
-    //     description: req.body.description,
-    //     media: req.files,
-    //     tags: req.body.tags
-    //     });
+    const media = []
+
+    const event = new Event({
+        user: req.body.payload,
+        name: req.body.name,
+        hours: req.body.hours,
+        start: req.body.start,
+        end: req.body.end,
+        location: req.body.location,
+        description: req.body.description,
+        media: media,
+        // tags: req.body.tags
+        });
     // await event.save();
     // await User.findOneAndUpdate({ _id: req.body.payload._id },
     // {$push: {events: event._id}}
     // );
     // res.json(event)
 
-    const imageFile = req.file;
-  
+    const imageFile = req.file!.path;
+// res.json(imageFile)
     // const media = [];
     // const image = imageFiles?.map(image => {
     //   return image.path
@@ -40,7 +43,7 @@ export const createEvent = async (
 
     cloudinary.uploader
         .upload(
-          imageFile!.path,
+          imageFile,
           {
             folder: "Fullstack/Event",
           },
@@ -48,23 +51,13 @@ export const createEvent = async (
             console.log(result, error)
           }
         ).then(async(result :any) => {
-          const event = new Event({
-                user: req.body.payload,
-                name: req.body.name,
-                hours: req.body.hours,
-                start: req.body.start,
-                end: req.body.end,
-                location: req.body.location,
-                description: req.body.description,
-                media: result.url,
-                tags: req.body.tags
-                });
-            await event.save();
-            await User.findOneAndUpdate({ _id: req.body.payload._id },
-            {$push: {events: event._id}}
-            );
-            res.json(event)
+          return new Promise((resolve, reject) => {
+            resolve(media.push(result.url))
+          })
+          // Promise.all(media).then(results => callback(results))
         })
+
+        res.json(media)
 
     // imageFiles?.forEach((el: any) => {
     //   console.log(el.path);
